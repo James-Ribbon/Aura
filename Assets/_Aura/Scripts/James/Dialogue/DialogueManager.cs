ca0154f60858ff3ffc7241085fcfea1daff6bdf6
James <JKR.Programming@gmail.com>
2025-04-07 20:55:08 +0100

    Object pooled dialogue boxes

---
+++ Assets/_Aura/Scripts/James/Dialogue/DialogueManager.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager Instance { get; private set; }
+
+    public ObjectPool dialogueBoxPool;
+
+    public Canvas worldCanvas;
+
+    [Header("Dialogue Settings")]
+    [SerializeField] private int maxActiveDialogues = 3;
+    [SerializeField] private float textSpeed = 0.05f;
+    [SerializeField] private float dialogueDuration = 3f;
+    public float textBoxOffset = 1.5f;
+
+    [Space(10)]
+
+    //[SerializeField] private GameObject dialogueBoxPrefab;
+
+    private GameObject dialogueBox;
+    private Transform currentTarget;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+    }
+
+    private void Update()
+    {
+        if (dialogueBox != null && dialogueBox.activeSelf && currentTarget != null)
+        {
+            Vector3 screenPosition = Camera.main.WorldToScreenPoint(
+                currentTarget.position + Vector3.up * textBoxOffset);
+
+            dialogueBox.transform.position = screenPosition;
+        }
+    }
+
+    public void ShowDialogue(Transform target, string message, float dialogueDuration)
+    {
+         /*dialogueBox = dialogueBoxPool.GetPooledObject();
+
+        if (dialogueBox != null)
+        {
+            currentTarget = target;
+            TMP_Text textComponent = dialogueBox.GetComponentInChildren<TMP_Text>();
+            textComponent.text = message;
+            dialogueBox.SetActive(true);
+        }*/
+         StartCoroutine(ShowDialogueCoroutine(target, message, dialogueDuration));
+        //GameObject dialogueBox = Instantiate(dialogueBoxPrefab, worldCanvas.transform);
+        //dialogueBox.GetComponent<DialogueBox>().Show(message, textSpeed, dialogueDuration);
+    }
+
+    public void HideDialogue()
+    {
+        if (dialogueBox != null)
+        {
+            dialogueBox.SetActive(false);
+        }
+
+        // You could trigger an "OnDialogueHidden" event here
+    }
+
+    IEnumerator ShowDialogueCoroutine(Transform target, string message, float dialogueDuration)
+    {
+        dialogueBox = dialogueBoxPool.GetPooledObject();
+        dialogueBox.transform.SetParent(worldCanvas.transform, false);
+
+        if (dialogueBox != null)
+        {
+            currentTarget = target;
+            TMP_Text textComponent = dialogueBox.GetComponentInChildren<TMP_Text>();
+            textComponent.text = message;
+            dialogueBox.SetActive(true);
+        }
+        yield return new WaitForSeconds(dialogueDuration);
+
+        HideDialogue();
+    }
+}
